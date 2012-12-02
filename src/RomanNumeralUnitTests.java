e14cd0ac937694a5cf51954affca8b0d2d1e7e0e
Reuven Yagel <robi@post.jce.ac.il>
2012-12-02 12:48:10 +0200

    REFACTOR test

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -5,15 +5,13 @@ import org.junit.Test;
 
 public class RomanNumeralUnitTests {
 
+	private RomanNumerals number = new RomanNumerals();
+
 	@Test
 	public void testThat1ReturnsI() {
-		RomanNumerals number = new RomanNumerals();
 		assertEquals("I", number.toRoman(1));
-	}
-	@Test
-	public void testThat2ReturnsII() {
-		RomanNumerals number = new RomanNumerals();
 		assertEquals("II", number.toRoman(2));
 	}
+	
 
 }
